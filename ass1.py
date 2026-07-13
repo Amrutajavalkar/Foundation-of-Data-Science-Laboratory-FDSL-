5bedb585874a7289d02350fe446a26c56f34ccee
Amrutajavalkar <javalkaramruta80@gmail.com>
2026-07-13 22:20:30 +0530

    ass1

---
+++ ass1.py
@@ -0,0 +1,21 @@
+import numpy as np
+#1D array
+arr1=np.array([1,2,3,4])
+arr1b=np.array([9,8,7,6])
+print(arr1)
+
+#2D array
+arr2=np.array([[1,2,3,4,5],[9,8,7,6,5]])
+print(arr2)
+
+#3D array
+arr3=np.array([[[11,22,33],[44,55,66]],[[99,88,77],[14,15,16]]])
+print(arr3)
+
+print(np.shape(arr1))
+print(np.shape(arr2))
+print(np.shape(arr3))
+
+print(arr1.dtype)
+
+
